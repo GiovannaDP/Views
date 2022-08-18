7509baeddf4e42412048123fa2d9c3d20eb23497
Giovanna Danelli <giovannadanellipau@gmail.com>
2022-08-17 21:55:30 -0300

    Acoes dos botoes salvar e limpar

--- app/src/main/java/com/example/views/MainActivity.java
+++ app/src/main/java/com/example/views/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.views.databinding.ActivityMainBinding;
@@ -11,12 +12,34 @@ import com.example.views.databinding.ActivityMainBinding;
 public class MainActivity extends AppCompatActivity {
     //Referencia para a classe de viewBinding
     private ActivityMainBinding activityMainBinding;
+    private Pessoa pessoa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         activityMainBinding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(activityMainBinding.getRoot());
+
+        activityMainBinding.salvarBt.setOnClickListener(view -> {
+            pessoa = new Pessoa(
+                    activityMainBinding.nomeEt.getText().toString(),
+                    activityMainBinding.sobrenomeEt.getText().toString(),
+                    activityMainBinding.emailEt.getText().toString(),
+                    activityMainBinding.estadoCivilSp.getSelectedItem().toString(),
+                    //((TextView) activityMainBinding.estadoCivilSp.getSelectedView()).getText().toString(),
+                    //activityMainBinding.femininoRb.isChecked()? "Feminino" : "Masculino"
+                    //activityMainBinding.femininoRb.isChecked()? activityMainBinding.femininoRb.getText().toString() : activityMainBinding.masculinoRb.getText().toString()
+                    activityMainBinding.femininoRb.isChecked()? getString(R.string.feminino) : getString(R.string.masculino)
+            );
+        });
+
+        activityMainBinding.limparBt.setOnClickListener(view -> {
+            activityMainBinding.nomeEt.setText("");
+            activityMainBinding.sobrenomeEt.setText("");
+            activityMainBinding.emailEt.setText("");
+            activityMainBinding.estadoCivilSp.setSelection(0);
+            activityMainBinding.femininoRb.setChecked(true);
+        });
     }
     
     public void onClickBotao(View botao){
